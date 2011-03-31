345f38dfb3dfd619b87f4709ab953bf5954e8566
Ali Neishabouri <m.neishabouri10@imperial.ac.uk>
2011-03-31 13:37:18 +0100

    Added the new transition matrix class to nettrader (in ntbpsrc)

---
+++ nettrader/ntbpsrc/ntbp_transition_rate_matrix_obj.h
@@ -0,0 +1,34 @@
+/*
+ * ntbp_transition_rate_matrix_obj.h
+ *
+ *  Created on: 30 Mar 2011
+ *      Author: man210
+ */
+
+#ifndef NTBP_TRANSITION_RATE_MATRIX_OBJ_H_
+#define NTBP_TRANSITION_RATE_MATRIX_OBJ_H_
+
+#include "nt_types.h"
+#include "boost/multi_array.hpp"
+
+class NTBP_transition_rate_matrix_o {
+public:
+	NTBP_transition_rate_matrix_o(NTsize numNewStates, NTreal min, NTreal max,
+			NTreal step);
+	virtual ~NTBP_transition_rate_matrix_o();
+	void setTransitionProbability(NTreal voltage, NTsize start, NTsize stop,
+			NTreal probability);
+	NTreal getTransitionProbability(NTreal voltage, NTsize start, NTsize stop);
+private:
+	typedef boost::multi_array<double, 3> matrix_array_type;
+	matrix_array_type _probMatrices;
+	void setTransitionProbability(NTsize index, NTsize start, NTsize stop,
+			NTreal probability);
+	NTreal getTransitionProbability(NTsize index, NTsize start, NTsize stop);
+
+	NTreal min;
+	NTreal max;
+	NTreal step;
+};
+
+#endif /* NTBP_TRANSITION_RATE_MATRIX_OBJ_H_ */
