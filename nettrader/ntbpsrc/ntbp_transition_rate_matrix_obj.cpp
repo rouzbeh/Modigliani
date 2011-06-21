f7b07c24e2f0c8835448c503be29ab217e7629e3
Ali Neishabouri <m.neishabouri10@imperial.ac.uk>
2011-06-21 11:33:14 +0100

    Removed useless methode Set probability by index. Corrected index calculation. Now rounding to nearest value.

--- nettrader/ntbpsrc/ntbp_transition_rate_matrix_obj.cpp
+++ nettrader/ntbpsrc/ntbp_transition_rate_matrix_obj.cpp
@@ -9,32 +9,29 @@
 
 NTBP_transition_rate_matrix_o::NTBP_transition_rate_matrix_o(
 		NTsize numNewStates, NTreal min, NTreal max, NTreal step) :
-	_probMatrices(
-			boost::extents[(max - min) / step + 10][numNewStates][numNewStates]) {
-	this->min=min;
-	this->max=max;
-	this->step=step;
+			_probMatrices(
+					boost::extents[floor((max - min) / step + 0.5)+1][numNewStates][numNewStates]) {
+	this->min = min;
+	this->max = max;
+	this->step = step;
 }
 
 NTBP_transition_rate_matrix_o::~NTBP_transition_rate_matrix_o() {
 	// TODO Auto-generated destructor stub
 }
 
-void NTBP_transition_rate_matrix_o::setTransitionProbability(NTreal voltage, NTsize start, NTsize stop,
-		NTreal probability) {
-	NTsize index = floor((voltage - min) / step);
-	setTransitionProbability(index, start, stop, probability);
+void NTBP_transition_rate_matrix_o::setTransitionProbability(NTreal voltage,
+		NTsize start, NTsize stop, NTreal probability) {
+	NTsize index = floor((voltage - min) / step + 0.5);
+	_probMatrices[index][start - 1][stop - 1] = probability;
 }
 
 NTreal NTBP_transition_rate_matrix_o::getTransitionProbability(NTreal voltage,
 		NTsize start, NTsize stop) {
-	NTsize index = floor((voltage - min) / step);
-	return _probMatrices[index][start - 1][stop - 1];
-}
-
-void NTBP_transition_rate_matrix_o::setTransitionProbability(NTsize index,
-		NTsize start, NTsize stop, NTreal probability) {
-	_probMatrices[index][start - 1][stop - 1] = probability;
+	//NTsize index = floor(voltage/step) - floor(min / step);
+	NTsize putative_index = floor((voltage - min) / step + 0.5);
+	//while(abs(voltage-_voltageIndex[putative_index])>step) putative_index++;
+	return _probMatrices[putative_index][start - 1][stop - 1];
 }
 
 NTreal NTBP_transition_rate_matrix_o::getTransitionProbability(NTsize index,
