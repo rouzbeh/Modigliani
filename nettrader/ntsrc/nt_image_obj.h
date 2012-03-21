3037c4a627b14c552c5e62322d906dad65675199
Ali Neishabouri <ali@neishabouri.net>
2012-03-21 15:48:59 +0000

    Remove warnings from nt_image_obj class files
    Explicitly mark unused variables
    Write a copy constructor
    Format

--- nettrader/ntsrc/nt_image_obj.h
+++ nettrader/ntsrc/nt_image_obj.h
@@ -20,20 +20,19 @@
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-
 /* $Id: nt_image_obj.h,v 1.1 2001/06/11 13:18:56 face Exp $
-* $Log: nt_image_obj.h,v $
-* Revision 1.1  2001/06/11 13:18:56  face
-* *** empty log message ***
-*
-* Revision 1.2  2000/10/03 08:30:03  face
-* *** empty log message ***
-*
-* Revision 1.1  2000/05/18 10:38:32  face
-* *** empty log message ***
-*
+ * $Log: nt_image_obj.h,v $
+ * Revision 1.1  2001/06/11 13:18:56  face
+ * *** empty log message ***
+ *
+ * Revision 1.2  2000/10/03 08:30:03  face
+ * *** empty log message ***
+ *
+ * Revision 1.1  2000/05/18 10:38:32  face
+ * *** empty log message ***
+ *
 
-*/
+ */
 
 #ifndef _nt_image_obj_h_
 #define _nt_image_obj_h_
@@ -49,68 +48,71 @@
 #include <valarray>
 #include <string>
 
-
-
-
 /** @short NT_image_o class
-\bug unknown
-\warning unknown
-*/
-class NT_image_o : public NT_o {
+ \bug unknown
+ \warning unknown
+ */
+class NT_image_o: public NT_o {
 public:
-    /***   Constructors, Copy/Assignment and Destructor  ***/
-    NT_image_o(NTsize width = 128, NTsize height = 64, NTsize colorDepth=16, NTimageDataFormat dataFormat = NT_RGBA);
-    NT_image_o(const NT_image_o & original);
-    const NT_image_o & operator= (const NT_image_o & right);
-    virtual ~NT_image_o();
-    /* ***  Methods              ***/
-    NTsize _width()  {
-        return width;
-    }
-    NTsize _height() {
-        return height;
-    }
-    NTsize _colorDepth() {
-        return 24;
-    }
-    NTimageDataFormat _dataFormat() {
-        return dataFormat;
-    }
+	/***   Constructors, Copy/Assignment and Destructor  ***/
+	NT_image_o(NTsize width = 128, NTsize height = 64, NTsize colorDepth = 16,
+			NTimageDataFormat dataFormat = NT_RGBA);
+	NT_image_o(const NT_image_o & original);
+	const NT_image_o & operator=(const NT_image_o & right);
+	virtual ~NT_image_o();
+	/* ***  Methods              ***/
+	NTsize _width() {
+		return width;
+	}
+	NTsize _height() {
+		return height;
+	}
+	NTsize _colorDepth() {
+		return 24;
+	}
+	NTimageDataFormat _dataFormat() {
+		return dataFormat;
+	}
 
-    NTreturn Load( string filename, NTimageFileType fileType = NT_AUTO);
-    NTreturn Save( string filename, NTimageFileType fileType = NT_AUTO) const {};
-    const valarray<NTsize>* GetImageArray() const {
-        return &imageVec;
-    }
+	NTreturn Load(string filename, NTimageFileType fileType = NT_AUTO);
+	NTreturn Save(__attribute__ ((unused)) string filename,
+			__attribute__ ((unused)) NTimageFileType fileType = NT_AUTO) const {
+		return NT_NOT_IMPLEMENTED;
+	}
+	;
+	const valarray<NTsize>* GetImageArray() const {
+		return &imageVec;
+	}
 //valarray<double>* GetRGBAImageArray() const;
-    /* ***  Data                 ***/
+	/* ***  Data                 ***/
 protected:
-    /* ***  Methods              ***/
-    NTreal _imageVecR( NTsize elementIndex ) const {
-        return imageVec[elementIndex]/255.0;
-    }
-    NTsize _imageVecI( NTsize elementIndex ) const {
-        return imageVec[elementIndex];
-    }
-    /* ***  Data                 ***/
-    NTsize width;
-    NTsize height;
-    NTsize colorDepth; /*  in bits per pixel without alpha channel {1,2,4,8,15,16,24,32}; */
-    NTimageDataFormat dataFormat;
+	/* ***  Methods              ***/
+	NTreal _imageVecR(NTsize elementIndex) const {
+		return imageVec[elementIndex] / 255.0;
+	}
+	NTsize _imageVecI(NTsize elementIndex) const {
+		return imageVec[elementIndex];
+	}
+	/* ***  Data                 ***/
+	NTsize width;
+	NTsize height;
+	NTsize colorDepth; /*  in bits per pixel without alpha channel {1,2,4,8,15,16,24,32}; */
+	NTimageDataFormat dataFormat;
 private:
-    /* ***  Methods              ***/
-    NTimageFileType Filename2Filetype( string filename );
-    NTreturn LoadBMP( string filename) {
-        cerr << "Not implemented."<<endl;
-    }
-    NTreturn LoadPPM( string filename) {
-        cerr << "Not implemented."<<endl;
-    }
-    NTreturn LoadTGA( string filename);
-    /* ***  Data                 ***/
-    valarray<NTsize> imageVec;
+	/* ***  Methods              ***/
+	NTimageFileType Filename2Filetype(string filename);
+	NTreturn LoadBMP(__attribute__ ((unused)) string filename) {
+		cerr << "Not implemented." << endl;
+		return NT_NOT_IMPLEMENTED;
+	}
+	NTreturn LoadPPM(__attribute__ ((unused)) string filename) {
+		cerr << "Not implemented." << endl;
+		return NT_NOT_IMPLEMENTED;
+	}
+	NTreturn LoadTGA(string filename);
+	/* ***  Data                 ***/
+	valarray<NTsize> imageVec;
 };
-
 
 #endif /* _nt_image_obj_h_ */
 
