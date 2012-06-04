d9d6711f631d20b505a0bcb4704d338de8eb546b
Ali Neishabouri <ali@neishabouri.net>
2012-06-04 17:07:37 +0100

    Renamed NTBP_Trans .. to Transition...

--- modigliani/mcore/Transition_rate_matrix.h
+++ modigliani/mcore/Transition_rate_matrix.h
@@ -1,28 +1,31 @@
 /*
- * ntbp_transition_rate_matrix.h
+ * transition_rate_matrix.h
  *
  *  Created on: 30 Mar 2011
  *      Author: man210
  */
 
-#ifndef NTBP_TRANSITION_RATE_MATRIX_OBJ_H_
-#define NTBP_TRANSITION_RATE_MATRIX_OBJ_H_
+#ifndef TRANSITION_RATE_MATRIX_H_
+#define TRANSITION_RATE_MATRIX_H_
 
 #include "mbase/types.h"
 // Boost's multi array class can be slow, if the data is not accessed properly.
 // See http://stackoverflow.com/questions/446866/boostmulti-array-performance-question
 
-class NTBP_transition_rate_matrix_o {
+namespace mcore {
+class Transition_rate_matrix {
 public:
-	NTBP_transition_rate_matrix_o(mbase::Size_t numNewStates, mbase::Real min, mbase::Real max,
-			mbase::Real step);
-	virtual ~NTBP_transition_rate_matrix_o();
-	void setTransitionProbability(mbase::Real voltage, mbase::Size_t start, mbase::Size_t stop,
-			mbase::Real probability);
-	void setTransitionProbability(mbase::Size_t index, mbase::Size_t start, mbase::Size_t stop,
-				mbase::Real probability);
-	mbase::Real getTransitionProbability(mbase::Real voltage, mbase::Size_t start, mbase::Size_t stop);
-	mbase::Real getTransitionProbability(mbase::Size_t index, mbase::Size_t start, mbase::Size_t stop);
+	Transition_rate_matrix(mbase::Size_t numNewStates, mbase::Real min,
+			mbase::Real max, mbase::Real step);
+	virtual ~Transition_rate_matrix();
+	void setTransitionProbability(mbase::Real voltage, mbase::Size_t start,
+			mbase::Size_t stop, mbase::Real probability);
+	void setTransitionProbability(mbase::Size_t index, mbase::Size_t start,
+			mbase::Size_t stop, mbase::Real probability);
+	mbase::Real getTransitionProbability(mbase::Real voltage,
+			mbase::Size_t start, mbase::Size_t stop);
+	mbase::Real getTransitionProbability(mbase::Size_t index,
+			mbase::Size_t start, mbase::Size_t stop);
 	mbase::Size_t get_index(mbase::Real voltage);
 private:
 	//typedef boost::multi_array<double, 3> matrix_array_type;
@@ -35,5 +38,6 @@ private:
 	mbase::Real step;
 	int num_states;
 };
+}
 
-#endif /* NTBP_TRANSITION_RATE_MATRIX_OBJ_H_ */
+#endif /* TRANSITION_RATE_MATRIX_H_ */
