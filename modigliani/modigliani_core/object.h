d8e16de6b0f86f7f83d44492a9ab88c9c9c7333f
Ali Neishabouri <ali@neishabouri.net>
2012-12-10 17:25:26 +0000

    Format

--- modigliani/modigliani_core/object.h
+++ modigliani/modigliani_core/object.h
@@ -41,7 +41,6 @@
 /* other includes */
 #include "../modigliani_base/physical_constants.h"
 
-
 /** @short Object class
  \bug unknown
  \warning unknown
@@ -65,66 +64,71 @@ enum StochasticType {
   BINOMIALPOPULATION
 };
 
-const modigliani_base::Real NTBP_STD_TEMPERATURE = 6.3; // Celsius (NOT TO BE CHANGED)
+const modigliani_base::Real NTBP_STD_TEMPERATURE = 6.3;  // Celsius (NOT TO BE CHANGED)
 
-modigliani_base::Real NTBP_temperature_rate_relation(modigliani_base::Real temp /* C */, modigliani_base::Real q10);
-modigliani_base::Real TemperatureRateRelation(modigliani_base::Real temp /* C */,
-    modigliani_base::Real baseTemp /* C */, modigliani_base::Real q10);
+modigliani_base::Real NTBP_temperature_rate_relation(
+    modigliani_base::Real temp /* C */, modigliani_base::Real q10);
+modigliani_base::Real TemperatureRateRelation(
+    modigliani_base::Real temp /* C */, modigliani_base::Real baseTemp /* C */,
+    modigliani_base::Real q10);
 /** Compute length constant in muMeter */
-modigliani_base::Real NTBP_length_constant_passive_cable(modigliani_base::Real diameter /* mu */,
-    modigliani_base::Real rAxoplasmic /* Ohm cm */, modigliani_base::Real gLeak /* mSiemens/cm^2 */);
+modigliani_base::Real NTBP_length_constant_passive_cable(
+    modigliani_base::Real diameter /* mu */,
+    modigliani_base::Real rAxoplasmic /* Ohm cm */,
+    modigliani_base::Real gLeak /* mSiemens/cm^2 */);
 
-class Object: public modigliani_base::Obj {
-public:
-	/***   Constructors, Copy/Assignment and Destructor  ***/
-	Object();
-	Object(const Object & original);
-	Object & operator=(const Object & right);
-	virtual ~Object();
-	/* ***  Methods              ***/
-	modigliani_base::Real _timeStep() const {
-		return (timeStep);
-	}
-	/** @short  update of explicitly time dependent variables using a deltaT = timeStep, NO update
-	 of dependent variables
-	 @param      none
-	 @return     none
-	 \warning    indirectly time dependent variables NOT updated
-	 \bug        unknown
-	 */
-	modigliani_base::Real update_timeStep(modigliani_base::Real newSuggestedTimeStep) { /* if (suggestedTimeStep > newSuggestedTimeStep) */
-		suggestedTimeStep = newSuggestedTimeStep;
-		return (newSuggestedTimeStep);
-	}
-	virtual modigliani_base::ReturnEnum Step() {
-		std::cerr
-				<< "Object:Step() - Warning : Non-overriden base method () called by derived class."
-				<< std::endl;
-		return (modigliani_base::ReturnEnum::NOT_IMPLEMENTED);
-	}
-	modigliani_base::ReturnEnum StepNTBP() {
-		timeStep = suggestedTimeStep;
-		return (modigliani_base::ReturnEnum::SUCCESS);
-	}
-	modigliani_base::Real _timeStep() {
-		return (timeStep);
-	}
-	modigliani_base::Real _baseTimeStep() {
-		return (baseTimeStep);
-	}
-	void setTimeStep(modigliani_base::Real newTimeStep) {
-		timeStep = newTimeStep;
-	}
-	/* ***  Data                 ***/
-protected:
-	/* ***  Methods              ***/
-	/* ***  Data                 ***/
-private:
-	/* ***  Methods              ***/
-	/* ***  Data                 ***/
-	modigliani_base::Real baseTimeStep; // time step size in msec;
-	static modigliani_base::Real timeStep; // time step size in msec;
-	static modigliani_base::Real suggestedTimeStep; // time step size in msec;
+class Object : public modigliani_base::Obj {
+  public:
+    /***   Constructors, Copy/Assignment and Destructor  ***/
+    Object();
+    Object(const Object & original);
+    Object & operator=(const Object & right);
+    virtual ~Object();
+    /* ***  Methods              ***/
+    modigliani_base::Real _timeStep() const {
+      return (timeStep);
+    }
+    /** @short  update of explicitly time dependent variables using a deltaT = timeStep, NO update
+     of dependent variables
+     @param      none
+     @return     none
+     \warning    indirectly time dependent variables NOT updated
+     \bug        unknown
+     */
+    modigliani_base::Real update_timeStep(
+        modigliani_base::Real newSuggestedTimeStep) { /* if (suggestedTimeStep > newSuggestedTimeStep) */
+      suggestedTimeStep = newSuggestedTimeStep;
+      return (newSuggestedTimeStep);
+    }
+    virtual modigliani_base::ReturnEnum Step() {
+      std::cerr
+          << "Object:Step() - Warning : Non-overriden base method () called by derived class."
+          << std::endl;
+      return (modigliani_base::ReturnEnum::NOT_IMPLEMENTED);
+    }
+    modigliani_base::ReturnEnum StepNTBP() {
+      timeStep = suggestedTimeStep;
+      return (modigliani_base::ReturnEnum::SUCCESS);
+    }
+    modigliani_base::Real _timeStep() {
+      return (timeStep);
+    }
+    modigliani_base::Real _baseTimeStep() {
+      return (baseTimeStep);
+    }
+    void setTimeStep(modigliani_base::Real newTimeStep) {
+      timeStep = newTimeStep;
+    }
+    /* ***  Data                 ***/
+  protected:
+    /* ***  Methods              ***/
+    /* ***  Data                 ***/
+  private:
+    /* ***  Methods              ***/
+    /* ***  Data                 ***/
+    modigliani_base::Real baseTimeStep;  // time step size in msec;
+    static modigliani_base::Real timeStep;  // time step size in msec;
+    static modigliani_base::Real suggestedTimeStep;  // time step size in msec;
 };
 }
 #endif /* _Object.h_ */ 
