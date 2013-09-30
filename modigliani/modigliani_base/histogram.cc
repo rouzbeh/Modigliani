c6e71e8ba55c1a9ddb0baf5aeb8f764214b19be0
Ali Neishabouri <ali@neishabouri.net>
2013-09-30 17:02:48 +0100

    cpplint the histogram class

--- modigliani/modigliani_base/histogram.cc
+++ modigliani/modigliani_base/histogram.cc
@@ -19,49 +19,47 @@
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-#include "histogram.h"
+#include "modigliani_base/histogram.h"
 
-using namespace modigliani_base;
+namespace modigliani_base {
 
-/* ***      CONSTRUCTORS    ***/
-/** Create a Histogram */
-Histogram::Histogram(Real minVal, Real maxVal,
-        Size numberOfBins) :
-        bins((int) numberOfBins) {
-    min = minVal;
-    max = maxVal;
-    numBins = numberOfBins;
-    binWidth = (fabs(max - min)) / numBins;
-    numDataPoints = 0;
-    numOutliers = 0;
-    M_ASSERT( min < max);
-    M_ASSERT( numberOfBins > 0);
+/** @brief Creates a Histogram
+ */
+Histogram::Histogram(Real minVal,
+                     Real maxVal,
+                     Size numberOfBins):bins(static_cast<int>(numberOfBins)) {
+  min = minVal;
+  max = maxVal;
+  numBins = numberOfBins;
+  binWidth = (fabs(max - min)) / numBins;
+  numDataPoints = 0;
+  numOutliers = 0;
+  M_ASSERT(min < max);
+  M_ASSERT(numberOfBins > 0);
 }
 
 /* ***      COPY AND ASSIGNMEM    ***/
-Histogram::Histogram(const Histogram & original) :
-        Obj() {
-    bins = original.bins;
-    min = original.min;
-    max = original.max;
-    numBins = original.numBins;
-    binWidth = original.binWidth;
-    numDataPoints = original.numDataPoints;
-    numOutliers = original.numOutliers;
+Histogram::Histogram(const Histogram & original):Obj() {
+  bins = original.bins;
+  min = original.min;
+  max = original.max;
+  numBins = original.numBins;
+  binWidth = original.binWidth;
+  numDataPoints = original.numDataPoints;
+  numOutliers = original.numOutliers;
 }
 
-const Histogram&
-Histogram::operator=(const Histogram & right) {
-    if (this == &right)
-        return (*this); // Gracefully handle self assignment
-    bins = right.bins;
-    min = right.min;
-    max = right.max;
-    numBins = right.numBins;
-    binWidth = right.binWidth;
-    numDataPoints = right.numDataPoints;
-    numOutliers = right.numOutliers;
-    return (*this);
+const Histogram & Histogram::operator=(const Histogram & right) {
+  if (this == &right)
+    return (*this);             // Gracefully handle self assignment
+  bins = right.bins;
+  min = right.min;
+  max = right.max;
+  numBins = right.numBins;
+  binWidth = right.binWidth;
+  numDataPoints = right.numDataPoints;
+  numOutliers = right.numOutliers;
+  return (*this);
 }
 
 /* ***      DESTRUCTOR        ***/
@@ -72,9 +70,9 @@ Histogram::~Histogram() {
 /** @short    Reset the histogram class to object creation values
  */
 void Histogram::Reset() {
-    bins.clear();
-    numDataPoints = 0;
-    numOutliers = 0;
+  bins.clear();
+  numDataPoints = 0;
+  numOutliers = 0;
 }
 
 /** @short      Insert the value in the appropriate bin,
@@ -86,54 +84,52 @@ void Histogram::Reset() {
  \bug        unknown
  */
 void Histogram::BinValue(Real value) {
-    numDataPoints++;
-    if ((value > max) || (value < min))
-        numOutliers++;
-    else
-        (bins[floor(0.5 + (value - min) / (Real) binWidth)]) += 1;
+  numDataPoints++;
+  if ((value > max) || (value < min))
+    numOutliers++;
+  else
+    (bins[floor(0.5 + (value - min) / (Real) binWidth)]) += 1;
 }
 
-std::vector<Real> Histogram::PDF() const {
-    std::vector<Real> probVec(numBins);
-    for (Size ll = 0; ll < numBins; ll++) {
-        probVec[ll] = ((Real) bins[ll]) / ((Real) numDataPoints);
-    }
-    return (probVec);
+std::vector < Real > Histogram::PDF() const {
+  std::vector < Real > probVec(numBins);
+  for (Size ll = 0; ll < numBins; ll++) {
+    probVec[ll] = ((Real) bins[ll]) / ((Real) numDataPoints);
+  }
+  return (probVec);
 }
 
 Real Histogram::ShannonEntropy() const {
-    Real h = 0;
-    Real p = 0;
-    for (Size ll = 0; ll < numBins; ll++) {
-        p = ((Real) bins[ll]) / ((Real) numDataPoints);
-        if (0.0 != p)
-            h -= p * log(p);
-    }
-    return (h / log(2.0));
+  Real h = 0;
+  Real p = 0;
+  for (Size ll = 0; ll < numBins; ll++) {
+    p = ((Real) bins[ll]) / ((Real) numDataPoints);
+    if (0.0 != p)
+      h -= p * log(p);
+  }
+  return (h / log(2.0));
 }
 
-/** @short
- @param      showHeader Show the header
+/**
+   @brief
+   @param showHeader Whether to show the header
  */
 void Histogram::Show(bool showHeader) const {
-    if (showHeader == true)
-        std::cout << "Histogram " << numBins << " bins [" << min << "," << max
-                << "] with " << numDataPoints << " of which " << numOutliers
-                << " outliers." << std::endl;
-    for (Size ll = 0; ll < numBins; ll++) {
-        std::cout << bins[ll] << " ";
-    }
-    std::cout << std::endl;
+  if (showHeader == true)
+    std::cout << "Histogram " << numBins << " bins [" << min << "," << max
+      << "] with " << numDataPoints << " of which " << numOutliers
+      << " outliers." << std::endl;
+  for (Size ll = 0; ll < numBins; ll++) {
+    std::cout << bins[ll] << " ";
+  }
+  std::cout << std::endl;
 }
 
-std::ostream& operator<<(std::ostream& str, const Histogram & self) {
-    for (Size ll = 0; ll < self.numBins; ll++) {
-        str << self.bins[ll] << "\n ";
-    }
-    return (str);
+std::ostream & operator<<(std::ostream & str, const Histogram & self) {
+  for (Size ll = 0; ll < self.numBins; ll++) {
+    str << self.bins[ll] << "\n ";
+  }
+  return (str);
 }
+}  // namespace modigliani_base
 
-/* ***  PROTECTED                         ***   */
-/* ***  PRIVATE                           ***   */
-
-/* File skeleton generated by GenMObj version 0.7. */
