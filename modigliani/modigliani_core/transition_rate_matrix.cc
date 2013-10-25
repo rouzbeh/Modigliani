72d8b7b5eb0a9c01a2b01c61036db50704782f81
Ali Neishabouri <ali@neishabouri.net>
2013-10-25 14:56:15 +0100

    Comments

--- modigliani/modigliani_core/transition_rate_matrix.cc
+++ modigliani/modigliani_core/transition_rate_matrix.cc
@@ -1,7 +1,24 @@
 /**
  * @file transition_rate_matrix.cc
- * @author Ali Neishabouri
+ * @brief Transition_rate_matrix class implementation
+ *
  * Copyright 2011 Ali Neishabouri
+ *
+ * @section LICENCE
+ * This file is part of Modigliani.
+ *
+ * Modigliani is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Modigliani is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Modigliani.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "modigliani_core/transition_rate_matrix.h"
--- modigliani/modigliani_core/transition_rate_matrix.h
+++ modigliani/modigliani_core/transition_rate_matrix.h
@@ -1,8 +1,24 @@
 /**
  * @file transition_rate_matrix.h
- * Transition_rate_matrix class declaration
- * @author Ali Neishabouri <ali@neishabouri.net>
+ * @brief Transition_rate_matrix class header
+ *
  * Copyright 2011 Ali Neishabouri
+ *
+ * @section LICENCE
+ * This file is part of Modigliani.
+ *
+ * Modigliani is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Modigliani is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Modigliani.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef MODIGLIANI_MODIGLIANI_CORE_TRANSITION_RATE_MATRIX_H_
